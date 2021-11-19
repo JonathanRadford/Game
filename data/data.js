70d3b40aaaf1a654c236491b9a5f4091f9eaca0b
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-19 10:45:37 +0000

    Created DATA file

---
+++ data/data.js
@@ -0,0 +1,98 @@
+ export const StoryLine = {
+  1: {
+    button1: "Here, take some gold",
+    button2: "Get away you filthy oaf",
+    header: `<h2>You meet a man in the woods who looks destitute and defeated. He begs you for money!</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path1.png">
+      <div id="welcome">
+      <h2 id="main-text"> "Please help. I've got naught but the clothes on my skin!</h2>
+      </div>`,
+  },
+
+  2: {
+    button1: "CURSE YOU, Cows are my mortal enemy!",
+    button2: "Thank you, Kind friend",
+    header: `<h2>You come across a lowly cowhand, who offers you the finest Milk in the land</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path2.png">
+      <div id="welcome">
+      <h2 id="main-text"> "Would you like to try the finest Milk this side of Codeshire?"</h2>
+      </div>`,
+  },
+
+  3: {
+    button1: "Go left",
+    button2: "Go right",
+    header: `<h2>The man accepts your gold, blesses you, then points in two directions.</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path1.png">
+      <div id="welcome">
+      <h2 id="main-text"> *whispers* "Go left to find beauty, go right to find power"</h2>
+      </div>`,
+  },
+
+  4: {
+    button1: "Go left",
+    button2: "Go right",
+    header: `<h2>The old man, terrified and upset, points you in two directions.</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path1.png">
+      <div id="welcome">
+      <h2 id="main-text"> "Go left to find power, go right to find beauty"</h2>
+      </div>`,
+  },
+
+  5: {
+    button1: "Bad milk gave me flatulence!",
+    button2: "It all began long ago...",
+    header: `<h2>The cowhand is dumfounded that a human could have a cow as a mortal enemy.</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path2.1.png">
+      <div id="welcome">
+      <h2 id="main-text"> "What couldst a cow have done to ye?"</h2>
+      </div>`,
+  },
+
+  6: {
+    button1: "*Continues drinking!*",
+    button2: "This will not end well for ye!",
+    header: `<h2>You take the bottle in haste without realising the cowhand has become a great Witch!"</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/path2.2.png">
+      <div id="welcome">
+      <h2 id="main-text"> "You FOOL! That milk? POISONED!" *Maniacal laugh*</h2>
+      </div>`,
+  },
+
+  7: {
+    button1: "...",
+    button2: "I will defeat you, you brute!",
+    header: `<h2>You witness a strong man throw rocks across the riverbed. He approaches...</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/daf.png">
+      <div id="welcome">
+      <h2 id="main-text"> "Care to challenge me and prove your mettle?"</h2>
+      </div>`,
+  },
+
+  8: {
+    button1: "Beauty",
+    button2: "Power!",
+    header: `<h2>After taking directions from the old man, you stumble upon a beautiful Elf </h2>`,
+    text: `<img id="old-man" src="./imagesfolder/right(evil).png">
+      <div id="welcome">
+      <h2 id="main-text">"I am Sona. What do you wish for?"</h2>
+      </div>`,
+  },
+
+  9: {
+    button1: "9",
+    button2: "9",
+    header: `<h2>9</h2>`,
+    text: `<img id="old-man" src="daf.png">
+      <div id="welcome">
+      <h2 id="main-text"> 9</h2>
+      </div>`,
+  },
+
+  10: {
+    button1: "10",
+    button2: "10",
+    header: `<h2>You have died of dysentery</h2>`,
+    text: `<img id="old-man" src="./imagesfolder/game_over.png">`,
+  },
+};
--- main.js
+++ main.js
@@ -2,114 +2,17 @@
 //get element from the HTML page by using innerHTML
 //create an object which stores all the story data from start to finish
 //call the specific story data when a button is pressed
+import {StoryLine} from "./data/data.js";
 
-let Welcome = document.getElementById("welcome");
-let inputName = document.getElementById("name-input");
-let button1 = document.getElementById("btn-one");
-let button2 = document.getElementById("btn-two");
-let text = document.getElementById("main-text");
-let gameArea = document.getElementById("game-section");
-let header = document.getElementById("heading");
+const Welcome = document.getElementById("welcome");
+const inputName = document.getElementById("name-input");
+const button1 = document.getElementById("btn-one");
+const button2 = document.getElementById("btn-two");
+const text = document.getElementById("main-text");
+const gameArea = document.getElementById("game-section");
+const header = document.getElementById("heading");
 const music = document.getElementById("myAudio");
-
-let StoryLine = {
-  1: {
-    button1: "Here, take some gold",
-    button2: "Get away you filthy oaf",
-    header: `<h2>You meet a man in the woods who looks destitute and defeated. He begs you for money!</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/path1.png">
-      <div id="welcome">
-      <h2 id="main-text"> "Please help. I've got naught but the clothes on my skin!</h2>
-      </div>`,
-  },
-
-  2: {
-    button1: "CURSE YOU, Cows are my mortal enemy!",
-    button2: "Thank you, Kind friend",
-    header: `<h2>You come across a lowly cowhand, who offers you the finest Milk in the land</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/path2.png">
-      <div id="welcome">
-      <h2 id="main-text"> "Would you like to try the finest Milk this side of Codeshire?"</h2>
-      </div>`,
-  },
-
-  3: {
-    button1: "Go left",
-    button2: "Go right",
-    header: `<h2>The man accepts your gold, blesses you, then points in two directions.</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/daf.png">
-      <div id="welcome">
-      <h2 id="main-text"> *whispers* "Go left to find beauty, go right to find power"</h2>
-      </div>`,
-  },
-
-  4: {
-    button1: "Go left",
-    button2: "Go right",
-    header: `<h2>The old man, terrified and upset, points you in two directions.</h2>`,
-    text: `<img id="old-man" src="daf.png">
-      <div id="welcome">
-      <h2 id="main-text"> "Go left to find power, go right to find beauty"</h2>
-      </div>`,
-  },
-
-  5: {
-    button1: "Bad milk gave me flatulence!",
-    button2: "It all began long ago...",
-    header: `<h2>The cowhand is dumfounded that a human could have a cow as a mortal enemy.</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/path2.1.png">
-      <div id="welcome">
-      <h2 id="main-text"> "What couldst a cow have done to ye?"</h2>
-      </div>`,
-  },
-
-  6: {
-    button1: "*Continues drinking!*",
-    button2: "This will not end well for ye!",
-    header: `<h2>You take the bottle in haste without realising the cowhand has become a great Witch!"</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/path2.2.png">
-      <div id="welcome">
-      <h2 id="main-text"> "You FOOL! That milk? POISONED!" *Maniacal laugh*</h2>
-      </div>`,
-  },
-
-  7: {
-    button1: "...",
-    button2: "I will defeat you, you brute!",
-    header: `<h2>You witness a strong man throw rocks across the riverbed. He approaches...</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/daf.png">
-      <div id="welcome">
-      <h2 id="main-text"> "Care to challenge me and prove your mettle?"</h2>
-      </div>`,
-  },
-
-  8: {
-    button1: "Beauty",
-    button2: "Power!",
-    header: `<h2>After taking directions from the old man, you stumble upon a beautiful Elf </h2>`,
-    text: `<img id="old-man" src="./imagesfolder/right(evil).png">
-      <div id="welcome">
-      <h2 id="main-text">"I am Sona. What do you wish for?"</h2>
-      </div>`,
-  },
-
-  9: {
-    button1: "9",
-    button2: "9",
-    header: `<h2>9</h2>`,
-    text: `<img id="old-man" src="daf.png">
-      <div id="welcome">
-      <h2 id="main-text"> 9</h2>
-      </div>`,
-  },
-
-  10: {
-    button1: "10",
-    button2: "10",
-    header: `<h2>You have died of dysentery</h2>`,
-    text: `<img id="old-man" src="./imagesfolder/game_over.png">`,
-  },
-};
+let newName;
 
 const prologue = () => {
   button2.addEventListener("click", prologue);
@@ -125,15 +28,13 @@ const getStart = () => {
   button1.innerHTML = "Path 1";
   button2.innerHTML = "Path 2";
   button1.addEventListener("click", () =>
-    getStage(1, () => {getStage(3, getStageDeath, getStageDeath);},() => {getStage(4, getStageSeven, getStageEight);},));
+    getStage(1, () => {getStage(3, getEnding2, getEnding2);},() => {getStage(4, getEnding2, getEnding2);},));
 
     button2.addEventListener("click", () => 
-    getStage(2, () => {getStage(5, getStageSuccess, getStageSuccess);}, () => {getStage(6, getStageDeath, getStageDeath);},));
-
+    getStage(2, () => {getStage(5, getEnding2, getEnding2);}, () => {getStage(6, getEnding2, getEnding2);},));
 }
 
 const getStage = (storyLineIndex, buttonOneAction, buttonTwoAction) => {
-  console.log("hit get stage");
   button1.innerHTML = `${StoryLine[storyLineIndex].button1}`;
   button2.innerHTML = `${StoryLine[storyLineIndex].button2}`;
   header.innerHTML = `${StoryLine[storyLineIndex].header}`;
@@ -142,7 +43,7 @@ const getStage = (storyLineIndex, buttonOneAction, buttonTwoAction) => {
   button2.addEventListener("click", buttonTwoAction);
 };
 
-function getStageSuccess() {
+const getEnding1 = () => {
   button1.innerHTML = `${StoryLine[9].button1}`;
   button2.innerHTML = `${StoryLine[9].button2}`;
   header.innerHTML = `${StoryLine[9].header}`;
@@ -151,7 +52,7 @@ function getStageSuccess() {
   button2.style = "display: none";
 }
 
-function getStageDeath() {
+const getEnding2 = () =>{
   button1.innerHTML = `${StoryLine[10].button1}`;
   button2.innerHTML = `${StoryLine[10].button2}`;
   header.innerHTML = `${StoryLine[10].header}`;
@@ -159,6 +60,8 @@ function getStageDeath() {
   button1.style = "display: none";
   button2.style = "display: none";
 }
+
+
 
 function playAudio() {
   music.play();
