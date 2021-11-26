b6acd71147ab499f64a417829733169e8d0f3bec
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-11-26 13:53:20 +0000

    (feat): Refactored code to discuss

--- data/data.js
+++ data/data.js
@@ -1,220 +1,248 @@
 export const StoryLine = {
   1: {
-    button1: "Here, take some gold",
-    button2: "Get away you filthy oaf",
+    button1: { buttonText: "Here, take some gold", takeMeTo: 2 },
+    button2: { buttonText: "Get away you filthy oaf", takeMeTo: 3 },
     header: `<h2>You meet a man in the woods who looks destitute. He begs you for money!</h2>`,
     text: `<img id="story-img" src="./imagesfolder/path1.png">
       <div id="welcome">
       <h2 id="main-text"> "Please help. I've got naught but the clothes on my skin!</h2>
       </div>`,
   },
-
   2: {
-    button1: "CURSE YOU, Cows are my mortal enemy!",
-    button2: "Thank you, Kind friend",
+    button1: { buttonText: "CURSE YOU, Cows are my mortal enemy!", takeMeTo: 1 },
+    button2: { buttonText: "Thank you, Kind friend", takeMeTo: 3 },
     header: `<h2>You come across a lowly cowhand, who offers you the finest Milk in the land</h2>`,
     text: `<img id="story-img" src="./imagesfolder/path2.png">
       <div id="welcome">
       <h2 id="main-text"> "Would you like to try the finest Milk this side of Codeshire?"</h2>
       </div>`,
   },
-
   3: {
-    button1: "Go left",
-    button2: "Go right",
+    button1: { buttonText: "Go left", takeMeTo: 1 },
+    button2: { buttonText: "Go right", takeMeTo: 2 },
     header: `<h2>The man accepts your gold, blesses you, then points in two directions.</h2>`,
     text: `<img id="story-img" src="./imagesfolder/man-whisper.png">
       <div id="welcome">
       <h2 id="main-text"> *whispers* "Go left to find beauty, go right to find power"</h2>
       </div>`,
   },
-
-  4: {
-    button1: "Go left",
-    button2: "Go right",
-    header: `<h2>The old man, terrified and upset, points you in two directions.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/man-scare.png">
-      <div id="welcome">
-      <h2 id="main-text"> "Go left to find power, go right to find beauty"</h2>
-      </div>`,
-  },
-
-  5: {
-    button1: "Milk gives me flatulence!",
-    button2: "It all began long ago...",
-    header: `<h2>The cowhand is dumfounded that a human could have a cow as a mortal enemy.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/path2.1.png">
-      <div id="welcome">
-      <h2 id="main-text"> "What couldst a cow have done to ye?"</h2>
-      </div>`,
-  },
-
-  6: {
-    button1: "*Continue drinking!*",
-    button2: "This will not end well for you!",
-    header: `<h2>You take the bottle in haste without realising the cowhand has become a great Witch!"</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/path2.2.png">
-      <div id="welcome">
-      <h2 id="main-text"> "You FOOL! That milk? POISONED!" *Maniacal laugh*</h2>
-      </div>`,
-  },
-
-  7: {
-    button1: "I dream of a better life. Wisdom",
-    button2: "I seek POWER!",
-    header: `<h2>You notice a wise man camping by a fire, stroking his beard. You approach. </h2>`,
-    text: `<img id="story-img" src="./imagesfolder/beard.png">
-      <div id="welcome">
-      <h2 id="main-text"> "I see you've made it this far. Do you seek wisdom or power?"</h2>
-      </div>`,
-  },
-
-  8: {
-    button1: "Beauty",
-    button2: "Power!",
-    header: `<h2>After taking directions from the old man, you stumble upon an enchanting Elf </h2>`,
-    text: `<img id="story-img" src="./imagesfolder/elf.png">
-      <div id="welcome">
-      <h2 id="main-text">"I am Sona. What do you wish for?"</h2>
-      </div>`,
-  },
-
-  9: {
-    button1: "*You continue pulling...*",
-    button2: "*You drink some refreshing river water*",
-    header: `<h2>Your conversation awkwardly ends and you leave in search of great power. You stumble upon a sword stuck in a pedestal.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/swordinstone.png">
-      <div id="welcome">
-      <h2 id="main-text">You stand tall and begin to pull the sword but there's incredible resistance.</h2>
-      </div>`,
-  },
-
-//  The cowhand realises that the true villain of this piece is Lactose Intolerance.
-
-  10: {
-    button1: "Take up arms, friend, for now we fight!",
-    button2: "Let me explain one more time...",
-    header: `<h2>You spend hours boring the cowhand with you stories. They become enraged and charge towards you!.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/charge.png">
-    <div id="welcome">
-    <h2 id="main-text">"I CAN'T TAKE THIS FOR A SECOND LONGER!</h2>
-    </div>`,
-  },
-  11: {
-    button1: "*Continue drinking!*",
-    button2: "*Throw the Milk at the witch!*",
-    header: `<h2>The Witch is flumuxed by your continous drinking of the sacred Milk!</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/path2.2.png">
-      <div id="welcome">
-      <h2 id="main-text">"This Milk should have killed you! How is this possible?"</h2>
-      </div>`,
-  },
-
-  12: {
-    button1: "This cow fued must end. Would you kindly kill this witch!",
-    button2: "*Throw your Milk at the Witch!*",
-    header: `<h2>The Witch calls upon the sacred Cows of Codeshire to descend from the mountains.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/cows.png">
-    <div id="welcome">
-    <h2 id="main-text">The cows, however, wait for you to speak.</h2>
-    </div>`,
-  },
-  13: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>The man nods with approvement.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/peace.png">
-    <div id="welcome">
-      <h2 id="main-text">"Let me give you some sagely advice. Life is often difficult, so dream big but think small. That way little victories bring joy."</h2>
-    </div>`,
-  },
-
-  14:{
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You seek power and the old man gives it to you, but absolute power corrupts absolutely and you become the villain of this story!</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/evil.png">
-    `,
-  }, 
-
-  15: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>The great Elf sees the kindness in your heart. She gives you the finest sword in all of Codeshire.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/sword.png">
-    <div id="welcome">
-      <h2 id="main-text">"It's dangerous to go alone. Take this, for you have the heart of a hero."</h2>
-    </div>`,
-  },
-
-  16: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You attempt to fight the great Elf but her powers are too much for you and she destroys you with a punishing blow. It's super effective</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/death.png">
-    <div id="welcome">
-      <h2 id="main-text">You have fainted.</h2>
-    </div>`,
-  },
-
-  17: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You have died of dysentery</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/game_over.png">
-    <div id="welcome">
-      <h2 id="main-text"></h2>
-    </div>`,
-  },
-
-  18: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You quickly gain the upper hand. As you rain blows upon the Cowhand you realise what your hatred has done to you. You're a monster!</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/evil.png">
-    <div id="welcome">
-      <h2 id="main-text">"We all make choices in life, but in the end our choices make us."</h2>
-    </div>`,
-  },
-
-
-  19: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You pull with all your might and the sword releases from the pedestal!</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/sword.png">
-    <div id="welcome">
-      <h2 id="main-text">Congratulations! You are the Hero of Codeshire!</h2>
-    </div>`,
-  },
-
-  20: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>You fend off the Cowhand with your words, your stories, your past deeds. You ponder what is better – to be born good, or to overcome your evil nature through great effort?</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/peace.png">
-    <div id="welcome">
-    <h2 id="main-text">Your story ends with a new friend made, and a new perspective.</h2>
-    </div>`,
-  },
-
-  21: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>The Sacred Cows of Codeshire turn on the Witch and tear them to pieces, before silently returning to the Mountains.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/war.png">
-    <div id="welcome">
-      <h2 id="main-text">Sadly,you have realised the udder madness of war.</h2>
-    </div>`,
-  },
-
-  22: {
-    button1: "Restart",
-    button2: "",
-    header: `<h2>The Witch is covered in the sacred Milk, and dies of Dysentry.</h2>`,
-    text: `<img id="story-img" src="./imagesfolder/ruler.png">
-    <div id="welcome">
-      <h2 id="main-text">You are now the leader of the sacred Cows of Codeshire! What a mooment!</h2>
-    </div>`,
-  },  
 };
+// export const StoryLine = {
+//   1: {
+//     button1: "Here, take some gold",
+//     button2: "Get away you filthy oaf",
+//     header: `<h2>You meet a man in the woods who looks destitute. He begs you for money!</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/path1.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "Please help. I've got naught but the clothes on my skin!</h2>
+//       </div>`,
+//   },
+
+//   2: {
+//     button1: "CURSE YOU, Cows are my mortal enemy!",
+//     button2: "Thank you, Kind friend",
+//     header: `<h2>You come across a lowly cowhand, who offers you the finest Milk in the land</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/path2.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "Would you like to try the finest Milk this side of Codeshire?"</h2>
+//       </div>`,
+//   },
+
+//   3: {
+//     button1: "Go left",
+//     button2: "Go right",
+//     header: `<h2>The man accepts your gold, blesses you, then points in two directions.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/man-whisper.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> *whispers* "Go left to find beauty, go right to find power"</h2>
+//       </div>`,
+//   },
+
+//   4: {
+//     button1: "Go left",
+//     button2: "Go right",
+//     header: `<h2>The old man, terrified and upset, points you in two directions.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/man-scare.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "Go left to find power, go right to find beauty"</h2>
+//       </div>`,
+//   },
+
+//   5: {
+//     button1: "Milk gives me flatulence!",
+//     button2: "It all began long ago...",
+//     header: `<h2>The cowhand is dumfounded that a human could have a cow as a mortal enemy.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/path2.1.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "What couldst a cow have done to ye?"</h2>
+//       </div>`,
+//   },
+
+//   6: {
+//     button1: "*Continue drinking!*",
+//     button2: "This will not end well for you!",
+//     header: `<h2>You take the bottle in haste without realising the cowhand has become a great Witch!"</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/path2.2.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "You FOOL! That milk? POISONED!" *Maniacal laugh*</h2>
+//       </div>`,
+//   },
+
+//   7: {
+//     button1: "I dream of a better life. Wisdom",
+//     button2: "I seek POWER!",
+//     header: `<h2>You notice a wise man camping by a fire, stroking his beard. You approach. </h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/beard.png">
+//       <div id="welcome">
+//       <h2 id="main-text"> "I see you've made it this far. Do you seek wisdom or power?"</h2>
+//       </div>`,
+//   },
+
+//   8: {
+//     button1: "Beauty",
+//     button2: "Power!",
+//     header: `<h2>After taking directions from the old man, you stumble upon an enchanting Elf </h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/elf.png">
+//       <div id="welcome">
+//       <h2 id="main-text">"I am Sona. What do you wish for?"</h2>
+//       </div>`,
+//   },
+
+//   9: {
+//     button1: "*You continue pulling...*",
+//     button2: "*You drink some refreshing river water*",
+//     header: `<h2>Your conversation awkwardly ends and you leave in search of great power. You stumble upon a sword stuck in a pedestal.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/swordinstone.png">
+//       <div id="welcome">
+//       <h2 id="main-text">You stand tall and begin to pull the sword but there's incredible resistance.</h2>
+//       </div>`,
+//   },
+
+// //  The cowhand realises that the true villain of this piece is Lactose Intolerance.
+
+//   10: {
+//     button1: "Take up arms, friend, for now we fight!",
+//     button2: "Let me explain one more time...",
+//     header: `<h2>You spend hours boring the cowhand with you stories. They become enraged and charge towards you!.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/charge.png">
+//     <div id="welcome">
+//     <h2 id="main-text">"I CAN'T TAKE THIS FOR A SECOND LONGER!</h2>
+//     </div>`,
+//   },
+//   11: {
+//     button1: "*Continue drinking!*",
+//     button2: "*Throw the Milk at the witch!*",
+//     header: `<h2>The Witch is flumuxed by your continous drinking of the sacred Milk!</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/path2.2.png">
+//       <div id="welcome">
+//       <h2 id="main-text">"This Milk should have killed you! How is this possible?"</h2>
+//       </div>`,
+//   },
+
+//   12: {
+//     button1: "This cow fued must end. Would you kindly kill this witch!",
+//     button2: "*Throw your Milk at the Witch!*",
+//     header: `<h2>The Witch calls upon the sacred Cows of Codeshire to descend from the mountains.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/cows.png">
+//     <div id="welcome">
+//     <h2 id="main-text">The cows, however, wait for you to speak.</h2>
+//     </div>`,
+//   },
+//   13: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>The man nods with approvement.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/peace.png">
+//     <div id="welcome">
+//       <h2 id="main-text">"Let me give you some sagely advice. Life is often difficult, so dream big but think small. That way little victories bring joy."</h2>
+//     </div>`,
+//   },
+
+//   14:{
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You seek power and the old man gives it to you, but absolute power corrupts absolutely and you become the villain of this story!</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/evil.png">
+//     `,
+//   },
+
+//   15: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>The great Elf sees the kindness in your heart. She gives you the finest sword in all of Codeshire.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/sword.png">
+//     <div id="welcome">
+//       <h2 id="main-text">"It's dangerous to go alone. Take this, for you have the heart of a hero."</h2>
+//     </div>`,
+//   },
+
+//   16: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You attempt to fight the great Elf but her powers are too much for you and she destroys you with a punishing blow. It's super effective</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/death.png">
+//     <div id="welcome">
+//       <h2 id="main-text">You have fainted.</h2>
+//     </div>`,
+//   },
+
+//   17: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You have died of dysentery</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/game_over.png">
+//     <div id="welcome">
+//       <h2 id="main-text"></h2>
+//     </div>`,
+//   },
+
+//   18: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You quickly gain the upper hand. As you rain blows upon the Cowhand you realise what your hatred has done to you. You're a monster!</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/evil.png">
+//     <div id="welcome">
+//       <h2 id="main-text">"We all make choices in life, but in the end our choices make us."</h2>
+//     </div>`,
+//   },
+
+//   19: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You pull with all your might and the sword releases from the pedestal!</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/sword.png">
+//     <div id="welcome">
+//       <h2 id="main-text">Congratulations! You are the Hero of Codeshire!</h2>
+//     </div>`,
+//   },
+
+//   20: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>You fend off the Cowhand with your words, your stories, your past deeds. You ponder what is better – to be born good, or to overcome your evil nature through great effort?</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/peace.png">
+//     <div id="welcome">
+//     <h2 id="main-text">Your story ends with a new friend made, and a new perspective.</h2>
+//     </div>`,
+//   },
+
+//   21: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>The Sacred Cows of Codeshire turn on the Witch and tear them to pieces, before silently returning to the Mountains.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/war.png">
+//     <div id="welcome">
+//       <h2 id="main-text">Sadly,you have realised the udder madness of war.</h2>
+//     </div>`,
+//   },
+
+//   22: {
+//     button1: "Restart",
+//     button2: "",
+//     header: `<h2>The Witch is covered in the sacred Milk, and dies of Dysentry.</h2>`,
+//     text: `<img id="story-img" src="./imagesfolder/ruler.png">
+//     <div id="welcome">
+//       <h2 id="main-text">You are now the leader of the sacred Cows of Codeshire! What a mooment!</h2>
+//     </div>`,
+//   },
+// };
--- main.js
+++ main.js
@@ -16,176 +16,207 @@ const header = document.getElementById("heading");
 const music = document.getElementById("myAudio");
 let newName;
 
-const playAudio = () => {
-  music.play();
-}
-
-//function that begins the Game
-const prologue = () => {
-  button2.addEventListener("click", rename);
-  button1.addEventListener("click", () => {
-    newName = inputName.value;
-    getStart();
-    return newName;
-  });
-};
-
-//takes the player to the path split and prints their name
-//button functions act on each new click by returning story information to the HTML
 const getStart = () => {
   welcome.innerHTML = "Welcome, " + newName + ". We shall begin your journey!";
   button1.innerHTML = "Path 1";
   button2.innerHTML = "Path 2";
-  button1.addEventListener("click", () =>
-    getStage(
-      1,
-      () => {
-        getStage(
-          3,
-          () => {
-            getStage(
-              7,
-              () => {
-                getEnding(13);
-              },
-              () => {
-                getEnding(14);
-              }
-            );
-          },
-          () => {
-            getStage(
-              8,
-              () => {
-                getEnding(15);
-              },
-              () => {
-                getEnding(16);
-              }
-            );
-          }
-        );
-      },
 
-      () => {
-        getStage(
-          4,
-          () => {
-            getStage(
-              7,
-              () => {
-                getEnding(13);
-              },
-              () => {
-                getEnding(14);
-              }
-            );
-          },
-          () => {
-            getStage(
-              8,
-              () => {
-                getEnding(15);
-              },
-              () => {
-                getEnding(16);
-              }
-            );
-          }
-        );
-      }
-    )
-  );
+  button1.onclick = () => {
+    const goTo = StoryLine[1].button1.takeMeTo;
+    getStage(goTo);
+  };
 
-  button2.addEventListener("click", () =>
-    getStage(
-      2,
-      () => {
-        getStage(
-          5,
-          () => {
-            getStage(
-              9,
-              () => {
-                getEnding(19);
-              },
-              () => {
-                getEnding(17);
-              }
-            );
-          },
-          () => {
-            getStage(
-              10,
-              () => {
-                getEnding(18);
-              },
-              () => {
-                getEnding(20);
-              }
-            );
-          }
-        );
-      },
-
-      () => {
-        getStage(
-          6,
-          () => {
-            getStage(
-              11,
-              () => {
-                getEnding(17);
-              },
-              () => {
-                getEnding(22);
-              }
-            );
-          },
-          () => {
-            getStage(
-              12,
-              () => {
-                getEnding(21);
-              },
-              () => {
-                getEnding(22);
-              }
-            );
-          }
-        );
-      }
-    )
-  );
+  button2.onclick = () => {
+    const goTo = StoryLine[1].button2.takeMeTo;
+    getStage(goTo);
+  };
 };
 
-const getStage = (storyLineIndex, buttonOneAction, buttonTwoAction) => {
-  button1.innerHTML = `${StoryLine[storyLineIndex].button1}`;
-  button2.innerHTML = `${StoryLine[storyLineIndex].button2}`;
-  header.innerHTML = `${StoryLine[storyLineIndex].header}`;
-  gameArea.innerHTML = `${StoryLine[storyLineIndex].text}`;
-  button1.addEventListener("click", buttonOneAction);
-  button2.addEventListener("click", buttonTwoAction);
-};
-
-const getEnding = (storyLineIndex) => {
-  button1.innerHTML = `${StoryLine[storyLineIndex].button1}`;
-  button2.innerHTML = `${StoryLine[storyLineIndex].button2}`;
-  header.innerHTML = `${StoryLine[storyLineIndex].header}`;
-  gameArea.innerHTML = `${StoryLine[storyLineIndex].text}`;
-  button1.addEventListener("click", refreshPage);
-  button2.style = "display: none";
-};
-
-const refreshPage = () => {
-  location.reload();
+const getStage = storyLineIndex => {
+  console.log(storyLineIndex);
+  const story = StoryLine[storyLineIndex];
+  button1.innerHTML = `${story.button1.buttonText}`;
+  button2.innerHTML = `${story.button2.buttonText}`;
+  header.innerHTML = `${story.header}`;
+  gameArea.innerHTML = `${story.text}`;
+  button1.onclick = () => getStage(story.button1.takeMeTo);
+  button2.onclick = () => getStage(story.button2.takeMeTo);
 };
 
 const rename = () => {
   inputName.value = "";
-  console.log("THIS");
 };
 
-//initial logic
-playAudio();
+//function that begins the Game
+const prologue = () => {
+  button2.onclick = rename;
+  button1.onclick = () => {
+    newName = inputName.value;
+    getStart();
+    return newName;
+  };
+};
+
 prologue();
+//takes the player to the path split and prints their name
+//button functions act on each new click by returning story information to the HTML
+// const getStart = () => {
+//   welcome.innerHTML = "Welcome, " + newName + ". We shall begin your journey!";
+//   button1.innerHTML = "Path 1";
+//   button2.innerHTML = "Path 2";
+//   button1.addEventListener("click", () =>
+//     getStage(
+//       1,
+//       () => {
+//         getStage(
+//           3,
+//           () => {
+//             getStage(
+//               7,
+//               () => {
+//                 getEnding(13);
+//               },
+//               () => {
+//                 getEnding(14);
+//               }
+//             );
+//           },
+//           () => {
+//             getStage(
+//               8,
+//               () => {
+//                 getEnding(15);
+//               },
+//               () => {
+//                 getEnding(16);
+//               }
+//             );
+//           }
+//         );
+//       },
+
+//       () => {
+//         getStage(
+//           4,
+//           () => {
+//             getStage(
+//               7,
+//               () => {
+//                 getEnding(13);
+//               },
+//               () => {
+//                 getEnding(14);
+//               }
+//             );
+//           },
+//           () => {
+//             getStage(
+//               8,
+//               () => {
+//                 getEnding(15);
+//               },
+//               () => {
+//                 getEnding(16);
+//               }
+//             );
+//           }
+//         );
+//       }
+//     )
+//   );
+
+//   button2.addEventListener("click", () =>
+//     getStage(
+//       2,
+//       () => {
+//         getStage(
+//           5,
+//           () => {
+//             getStage(
+//               9,
+//               () => {
+//                 getEnding(19);
+//               },
+//               () => {
+//                 getEnding(17);
+//               }
+//             );
+//           },
+//           () => {
+//             getStage(
+//               10,
+//               () => {
+//                 getEnding(18);
+//               },
+//               () => {
+//                 getEnding(20);
+//               }
+//             );
+//           }
+//         );
+//       },
+
+//       () => {
+//         getStage(
+//           6,
+//           () => {
+//             getStage(
+//               11,
+//               () => {
+//                 getEnding(17);
+//               },
+//               () => {
+//                 getEnding(22);
+//               }
+//             );
+//           },
+//           () => {
+//             getStage(
+//               12,
+//               () => {
+//                 getEnding(21);
+//               },
+//               () => {
+//                 getEnding(22);
+//               }
+//             );
+//           }
+//         );
+//       }
+//     )
+//   );
+// };
+
+// const getStage = (storyLineIndex, buttonOneAction, buttonTwoAction) => {
+//   button1.innerHTML = `${StoryLine[storyLineIndex].button1}`;
+//   button2.innerHTML = `${StoryLine[storyLineIndex].button2}`;
+//   header.innerHTML = `${StoryLine[storyLineIndex].header}`;
+//   gameArea.innerHTML = `${StoryLine[storyLineIndex].text}`;
+//   // button1.removeEventListener("click", buttonOneAction);
+//   // console.dir(button1);
+//   button1.onclick = ("click", buttonOneAction);
+
+//   console.dir(button1);
+
+//   button2.addEventListener("click", buttonTwoAction);
+// };
+
+// const getEnding = storyLineIndex => {
+//   button1.innerHTML = `${StoryLine[storyLineIndex].button1}`;
+//   button2.innerHTML = `${StoryLine[storyLineIndex].button2}`;
+//   header.innerHTML = `${StoryLine[storyLineIndex].header}`;
+//   gameArea.innerHTML = `${StoryLine[storyLineIndex].text}`;
+//   button1.addEventListener("click", refreshPage);
+//   button2.style = "display: none";
+// };
+
+// const playAudio = () => {
+//   music.play();
+// };
+
+// const refreshPage = () => {
+//   location.reload();
+// };
+
+// //initial logic
+// // playAudio();
